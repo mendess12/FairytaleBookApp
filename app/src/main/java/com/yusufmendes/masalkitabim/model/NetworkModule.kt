027c83146b5105a1b70785ccf389a24edf755485
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-08-24 15:46:35 +0300

    Add network module

---
+++ app/src/main/java/com/yusufmendes/masalkitabim/model/NetworkModule.kt
@@ -0,0 +1,17 @@
+package com.yusufmendes.masalkitabim.model
+
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun firebaseFireStore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+}
