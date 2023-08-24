3487eeb3a59d93d2f4b00c16d83c822800ed4f49
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-08-24 15:47:41 +0300

    Add safe args and admob

--- app/src/main/java/com/yusufmendes/masalkitabim/view/detail/DetailFragment.kt
+++ app/src/main/java/com/yusufmendes/masalkitabim/view/detail/DetailFragment.kt
@@ -3,16 +3,43 @@ package com.yusufmendes.masalkitabim.view.detail
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.yusufmendes.masalkitabim.R
 import com.yusufmendes.masalkitabim.databinding.FragmentDetailBinding
+import com.yusufmendes.masalkitabim.util.downloadFromUrl
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     private lateinit var binding: FragmentDetailBinding
+    private val args: DetailFragmentArgs by navArgs()
+    lateinit var mAdView: AdView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailBinding.bind(view)
+
+        adMob()
+
+        binding.apply {
+            detailScreenToolBar.toolBarNameTv.text = args.fairytale.name.toString()
+            detailScreenFairytaleTv.text = args.fairytale.fairytale.toString()
+            detailScreenIv.downloadFromUrl(args.fairytale.imageUrl.toString())
+            detailScreenToolBar.toolBarBackIv.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
     }
 
+    private fun adMob() {
+        MobileAds.initialize(requireContext())
+        mAdView = binding.adView2
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+    }
 }
