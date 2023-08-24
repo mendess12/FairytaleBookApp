98673cea6e5e5dea292c764563f88d73f93cf786
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-08-24 15:36:42 +0300

    Add data model

---
+++ app/src/main/java/com/yusufmendes/masalkitabim/model/Fairytale.kt
@@ -0,0 +1,13 @@
+package com.yusufmendes.masalkitabim.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Fairytale
+    (
+    val name: String? = null,
+    val fairytale: String? = null,
+    val imageUrl: String? = null
+) :
+    Parcelable
