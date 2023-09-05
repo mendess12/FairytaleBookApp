e7d071a2965e697c6220d3e12ac0969bb0402356
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-09-05 19:50:42 +0300

    Update repository name

--- app/src/main/java/com/yusufmendes/masalkitabim/repository/FairytaleRepositoryImpl.kt
+++ app/src/main/java/com/yusufmendes/masalkitabim/repository/FairytaleRepositoryImpl.kt
@@ -1,14 +1,18 @@
 package com.yusufmendes.masalkitabim.repository
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.yusufmendes.masalkitabim.domain.repos.FairytaleRepository
 import com.yusufmendes.masalkitabim.model.Fairytale
 import com.yusufmendes.masalkitabim.util.FairytaleConst
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class FairytaleRepository @Inject constructor(private val firebaseDatabaseStore: FirebaseFirestore) {
+@Singleton
+class FairytaleRepositoryImpl @Inject constructor(private val firebaseDatabaseStore: FirebaseFirestore) :
+    FairytaleRepository {
 
-    suspend fun getFairytaleList(): List<Fairytale> {
+    override suspend fun getFairytaleList(): List<Fairytale> {
         return firebaseDatabaseStore.collection(FairytaleConst.FAIRYTALE)
             .get()
             .await()
